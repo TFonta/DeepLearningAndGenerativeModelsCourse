ec4e9a943f145188e00fbee27127c34daac207bc
TFonta <tomaso.fontanini@gmail.com>
2024-12-10 15:17:04 +0100

    dlagm 2024

--- hpc_test/hpc_DLAGM.bash
+++ hpc_test/hpc_DLAGM.bash
@@ -8,9 +8,8 @@
 #SBATCH --ntasks-per-node 10
 
 
-#< Charge resources to account
-#SBATCH --reservation=t_2023_dlagm_20231214    
-#SBATCH --account t_2023_dlagm
+#< Charge resources to account   
+#SBATCH --account t_2024_dlagm
 
 echo $SLURM_JOB_NODELIST
 
@@ -18,7 +17,7 @@ echo  #OMP_NUM_THREADS : $OMP_NUM_THREADS
 
 module load miniconda3
 source "$CONDA_PREFIX/etc/profile.d/conda.sh"
-conda activate pytorch-cuda-11.1
+conda activate stylegan
 
 python ./hpc_test/hpc_test.py --dataset_path /hpc/archive/T_2023_DLAGM/tomaso.fontanini/cifar10 \
 --checkpoint_path /hpc/group/T_2023_DLAGM/tomaso.fontanini/hpc_exp
